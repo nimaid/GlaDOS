e76f6edb58c7360e04564118d02c32ba11321b57
Ella Jameson <ellagjameson@gmail.com>
2025-01-20 01:18:19 -0700

    Trying to fix the goto ends

--- start_windows.bat
+++ start_windows.bat
@@ -11,20 +11,25 @@ if exist ".venv\" (
     echo Found .venv, activating...
     call  .venv\Scripts\activate
     python !SCRIPT!
+    goto END
 else (
     echo Could not find .venv, looking for conda...
     where conda >nul 2>nul
-    if !errorlevel! EQU 0 (
+    if errorlevel 0 (
         echo Found conda, checking for a virtual environment named glados...
         conda list --name glados >nul 2>nul
-        if !errorlevel! EQU 0 (
+        if errorlevel 0 (
             call conda run -n glados python !SCRIPT!
+            goto END
         ) else (
             echo Could not find a conda virtual environemnt named glados^!
-            pause
+            goto END
         )
     ) else (
         echo Could not find a a conda installation^!
-        pause
+        goto END
     )
 )
+
+END:
+pause
